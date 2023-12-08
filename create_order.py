ea244b57b387dbd5bcbd5240d64520570aa5f587
alekseykostsov <akoscov1@gmail.com>
2023-12-08 02:45:53 +0200

    final

--- create_order.py
+++ create_order.py
@@ -8,13 +8,7 @@ def post_new_order(body):
 
 response = post_new_order(data.body);
 order_num = response.json()["track"]
-# print(response)
-# print(order_num)
+
 
 def order_search (num):
     return requests.get(configuration.URL_SERVICE + configuration.number_order + str(num))
-
-# order = order_search(order_num)
-# print(order.status_code)
-
-
